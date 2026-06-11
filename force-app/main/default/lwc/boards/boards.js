d95f6a4c395691dcb0b2927e08d83f5413707191
ajaybeeravelli <127074203+ajaybeeravelli@users.noreply.github.com>
2026-06-11 16:44:23 +0530

    form

--- force-app/main/default/lwc/boards/boards.js
+++ force-app/main/default/lwc/boards/boards.js
@@ -1,13 +1,26 @@
 import { LightningElement } from 'lwc';
-    const COLUMNS=[
+import BOARDOBJECTAPINAME from '@salesforce/resourceUrl/Board__c';
+import NAME_FIELD from '@salesforce/schema/Board__c.Name';
+import DESCRIPTION_FIELD from '@salesforce/schema/Board__c.Description__c';
+import NO_OF_SECTIONS_FIELD from '@salesforce/schema/Board__c.NoOfSections__c';
+const COLUMNS=[
         {label:'Board Name',fieldName:'name'},
         {label:'Description',fieldName:'description'},
         {label:'sections',fieldName:'sections'},
         {type:'button',typeAttributes:{label:'View Board',name:'view_board'}}
     ];
 export default class Boards extends LightningElement {
+    recordId;
+    boardObjectApiName = BOARDOBJECTAPINAME;
+    boardNameField = NAME_FIELD;
+    boardDescriptionField = DESCRIPTION_FIELD;
+    boardNoOfSectionsField = NO_OF_SECTIONS_FIELD;
     columns = COLUMNS;
+    showModalPopup = false;
+    closePopupHandler(event){
+        this.showModalPopup = false;
+    }
     handleAddBoard(event){
-
+        this.showModalPopup = true;
     }
 }
